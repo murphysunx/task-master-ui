8cebd4f3c84575eab335712555d9affb0ed73b52
Murphy <murphysunx@gmail.com>
2024-07-29 22:28:31 +1000

    chore: Update ESLint configuration to include Storybook plugin and add Storybook files

---
+++ src/app/tasks/task-card.stories.tsx
@@ -0,0 +1,33 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import TaskCard from "./task-card";
+
+const meta = {
+  component: TaskCard,
+} satisfies Meta<typeof TaskCard>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const todo: Story = {
+  args: {
+    task: {
+      id: "1",
+      title: "Task 1",
+    },
+  },
+};
+
+export const Completed: Story = {
+  args: {
+    task: {
+      id: "1",
+      title: "Task 1",
+      completed: true,
+      description: "sflwsjlfkjsldf",
+    },
+
+    showDescription: false,
+  },
+};
