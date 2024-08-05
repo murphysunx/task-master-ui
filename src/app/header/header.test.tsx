2847d897b796ab9d8505df4983b7eda061ded921
Murphy <murphysunx@gmail.com>
2024-08-05 19:21:22 +1000

    chore: add header template

--- src/app/header/header.test.tsx
+++ src/app/header/header.test.tsx
@@ -1,9 +1,5 @@
-import { beforeEach, describe, expect, test, vi } from "vitest";
-import AuthStore from "../auth/stores/authStore";
-import { render, screen } from "@testing-library/react";
-import { Header } from "./header";
+import { describe, vi } from "vitest";
 import login from "../auth/apis/login";
-import { IUser } from "../auth/interfaces/user";
 
 vi.mock(import("../auth/apis/login"), async (importOrigingal) => {
   const origingal = await importOrigingal();
@@ -15,31 +11,4 @@ vi.mock(import("../auth/apis/login"), async (importOrigingal) => {
 
 const mockLogin = vi.mocked(login);
 
-describe("header", () => {
-  let authStore: AuthStore;
-
-  beforeEach(() => {
-    authStore = new AuthStore();
-  });
-
-  test("should have a login button when not logging in", () => {
-    render(<Header auth={authStore} />);
-    expect(screen.getByTestId("login-button")).not.toBeNull();
-  });
-
-  test("should have a logout button when logged in", async () => {
-    const email = "test@gmail.com";
-    const passwords = "123456";
-    const loggedInUser: IUser = {
-      id: "1",
-      email,
-      name: "test",
-      createdAt: new Date().toISOString(),
-      updatedAt: new Date().toISOString(),
-    };
-    mockLogin.mockResolvedValueOnce(loggedInUser);
-    await authStore.login(email, passwords);
-    render(<Header auth={authStore} />);
-    expect(screen.getByTestId("logout-button")).not.toBeNull();
-  });
-});
+describe("header", () => {});
