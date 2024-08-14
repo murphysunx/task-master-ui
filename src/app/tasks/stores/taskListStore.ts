d4f078f606cf0642a80356ddcf4f2ffd9d5e6ae0
Murphy <murphysunx@gmail.com>
2024-08-14 23:01:49 +1000

    fix: set default userId for now when createTask

--- src/app/tasks/stores/taskListStore.ts
+++ src/app/tasks/stores/taskListStore.ts
@@ -73,7 +73,7 @@ export default class TaskListStore {
       body: JSON.stringify({
         title,
         description,
-        taskListId: this.id,
+        userId: 1,
       }),
     });
     if (response.ok) {
